ddb6f554d21a22d2de955baba2969e75ebf03cf8
Vadim Zaigrin <vzaigrin@yandex.ru>
2020-12-22 21:11:09 +0300

    Update Solution for Lesson 2

--- scala-dev-mooc/src/main/scala/module1/02-fp.scala
+++ scala-dev-mooc/src/main/scala/module1/02-fp.scala
@@ -26,6 +26,15 @@ import scala.annotation.tailrec
        case Option.Some(v) => v
        case Option.None => throw new Exception("get on empty Option")
       }
+
+    /**
+     *
+     * реализовать метод orElse который будет возвращать другой Option, если данный пустой
+     */
+    def orElse[B >: A](that: => Option[B]): Option[B] = this match {
+      case Option.Some(_) => this
+      case Option.None => that
+    }
    }
 
    object Option {
@@ -34,24 +43,12 @@ import scala.annotation.tailrec
    }
 
 
-
-
   /**
    *
    * Реализовать метод printIfAny, который будет печатать значение, если оно есть
    */
   def printIfAny[A](option: Option[A]): Unit = {
     if (!option.isEmpty) println(option.get)
-  }
-
-
-  /**
-   *
-   * реализовать метод orElse который будет возвращать другой Option, если данный пустой
-   */
-  def orElse[A](option1: Option[A], option2: Option[A]): Option[A] = {
-    if (option1.isEmpty) option2
-    else option1
   }
 
 
@@ -84,7 +81,8 @@ import scala.annotation.tailrec
    * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
    */
   def zip[A](option1: Option[A], option2: Option[A]): Option[(A, A)] =
-    Option.Some((option1.get, option2.get))
+    if (!option1.isEmpty && !option2.isEmpty) Option.Some((option1.get, option2.get))
+    else Option.None
 
 
   /**
@@ -92,10 +90,9 @@ import scala.annotation.tailrec
    * Реализовать метод filter, который будет возвращать не пустой Option
    * в случае если исходный не пуст и предикат от значения = true
    */
-  def filter[A](option: Option[A], p: Option[A] => Boolean): Option[A] = {
-    if (!option.isEmpty && p(option)) option
+  def filter[A](option: Option[A], p: A => Boolean): Option[A] =
+    if (!option.isEmpty && p(option.get)) option
     else Option.None
-  }
 
  }
 
@@ -211,18 +208,14 @@ import scala.annotation.tailrec
     *
     * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
     */
-   def append[T](list: List[T], last: T): List[T] = {
-     list match {
-       case h :: List.Nil => Cons(h, List[T](last))
-       case h :: t => Cons(h, append(t, last))
-     }
-   }
-
    def reverse[A](list: List[A]): List[A] = {
-     list match {
-       case h :: Nil => List[A](h)
-       case h :: t => append(reverse(t), h)
-     }
+     @tailrec
+     def loop(list: List[A], rev: List[A]): List[A] =
+       list match {
+         case h :: Nil => Cons(h, rev)
+         case h :: tail => loop(tail, Cons(h, rev))
+       }
+     loop(list, List[A]())
    }
 
 
@@ -231,25 +224,14 @@ import scala.annotation.tailrec
     * Написать функцию incList котрая будет принимать список Int и возвращать список,
     * где каждый элемент будет увеличен на 1
     */
-   def incList(list: List[Int]): List[Int] = {
-     list match {
-       case h :: List.Nil => List[Int](h + 1)
-       case h :: t => Cons(h + 1, incList(t))
-     }
-   }
-
+   def incList(list: List[Int]): List[Int] = map(list, (_: Int) + 1)
 
    /**
     *
     * Написать функцию shoutString котрая будет принимать список String и возвращать список,
     * где к каждому элементу будет добавлен префикс в виде '!'
     */
-   def shoutString(list: List[String]): List[String] = {
-     list match {
-       case h :: List.Nil => List[String]("!" + h)
-       case h :: t => Cons("!" + h, shoutString(t))
-     }
-   }
+   def shoutString(list: List[String]): List[String] = map(list, "!" + (_: String))
 
 
    /**
@@ -257,10 +239,13 @@ import scala.annotation.tailrec
     * Реализовать метод для списка который будет применять некую ф-цию к элементам данного списка
     */
    def map[T](list: List[T], f: T => T): List[T] = {
-     list match {
-       case h :: List.Nil => List[T](f(h))
-       case h :: t => Cons(f(h), map(t, f))
-     }
+     @tailrec
+     def loop(left: List[T], right: List[T]): List[T] =
+       left match {
+         case h :: List.Nil => Cons(f(h), right)
+         case h :: tail => loop(tail, Cons(f(h), right))
+       }
+     reverse(loop(list, List[T]()))
    }
 
  }
